fd66ead9c41b056c92ac4ba8dee0d16b83c7ccc7
Frank Lam <franlam55@gmail.com>
2021-08-16 16:23:22 +0100

    add filter by month component

--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import AddAttData from "../components/AddAttData";
 import Header from "../components/Header";
 import AttDataList from "../components/AttDataList";
+import MonthFilter from "../components/MonthFilter";
 
 const home = () => {
   return (
     <div>
       <Header />
       <AddAttData />
+      <MonthFilter />
       <AttDataList />
     </div>
   );
