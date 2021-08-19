d037ee0cbdd41b3e7223aecfed0ae87f5d526727
ltk55 <franklam@live.hk>
2021-08-19 17:43:15 +0100

    heroku deployment

--- server/db/index.js
+++ server/db/index.js
@@ -1,8 +1,13 @@
 import pkg from "pg";
+const isProduction = process.env.NODE_ENV === "production";
+const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
 const { Pool } = pkg;
 
-const pool = new Pool();
+const pool = new Pool({
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
+});
 
 export default {
   query: (text, params) => pool.query(text, params),
