ee7f8775d558f4bef54c941f73ea6fa2d22cf2bb
Frank Lam <franlam55@gmail.com>
2021-09-02 17:13:18 +0100

    Style: Add responsiveness to AddAttData component

--- client/src/components/AddAttData.js
+++ client/src/components/AddAttData.js
@@ -27,67 +27,65 @@ const AddAttData = () => {
   };
 
   return (
-    <div>
-      <form action="">
-        <div className="row mb-5">
-          <div className="col align-self-end">
-            <label htmlFor="date" className="form-label text-light">
-              Date
-            </label>
-            <input
-              value={attDate}
-              onChange={(e) => setAttDate(e.target.value)}
-              type="date"
-              className="form-control text-center"
-              placeholder="YYYYMMDD"
-            />
-          </div>
-          <div className="col align-self-end">
-            <label htmlFor="start-time" className="form-label text-light">
-              Start Time
-            </label>
-            <input
-              value={startTime}
-              onChange={(e) => setStartTime(e.target.value)}
-              type="time"
-              className="form-control text-center"
-              placeholder="HH:MM"
-            />
-          </div>
-          <div className="col align-self-end">
-            <label htmlFor="end-time" className="form-label text-light">
-              End Time
-            </label>
-            <input
-              value={endTime}
-              onChange={(e) => setEndTime(e.target.value)}
-              type="time"
-              className="form-control text-center"
-              placeholder="HH:MM"
-            />
-          </div>
-          <div className="col align-self-end">
-            <label htmlFor="lunch-time" className="form-label text-light">
-              Lunch Duration (min)
-            </label>
-            <input
-              value={lunchMin}
-              onChange={(e) => setLunchMin(e.target.value)}
-              type="number"
-              min="0"
-              step="1"
-              className="form-control text-center"
-              placeholder="MM"
-            />
-          </div>
-          <div className="col align-self-end">
-            <button onClick={handleSubmit} className="btn btn-primary ">
-              <span className="bg-primary">Submit</span>
-            </button>
-          </div>
+    <form action="" className="">
+      <div className="row mb-5">
+        <div className="col-md-3 align-self-end mb-3">
+          <label htmlFor="date" className="form-label text-light">
+            Date
+          </label>
+          <input
+            value={attDate}
+            onChange={(e) => setAttDate(e.target.value)}
+            type="date"
+            className="form-control text-center"
+            placeholder="YYYYMMDD"
+          />
         </div>
-      </form>
-    </div>
+        <div className="col-md-3 align-self-end mb-3">
+          <label htmlFor="start-time" className="form-label text-light">
+            Start Time
+          </label>
+          <input
+            value={startTime}
+            onChange={(e) => setStartTime(e.target.value)}
+            type="time"
+            className="form-control text-center"
+            placeholder="HH:MM"
+          />
+        </div>
+        <div className="col-md-3 align-self-end mb-3">
+          <label htmlFor="end-time" className="form-label text-light">
+            End Time
+          </label>
+          <input
+            value={endTime}
+            onChange={(e) => setEndTime(e.target.value)}
+            type="time"
+            className="form-control text-center"
+            placeholder="HH:MM"
+          />
+        </div>
+        <div className="col-md-2 align-self-end mb-3">
+          <label htmlFor="lunch-time" className="form-label text-light">
+            Lunch Duration (min)
+          </label>
+          <input
+            value={lunchMin}
+            onChange={(e) => setLunchMin(e.target.value)}
+            type="number"
+            min="0"
+            step="1"
+            className="form-control text-center"
+            placeholder="MM"
+          />
+        </div>
+        <div className="col-md-1 align-self-end mb-3">
+          <button onClick={handleSubmit} className="btn btn-primary ">
+            <span className="bg-primary">Submit</span>
+          </button>
+        </div>
+      </div>
+    </form>
   );
 };
 
