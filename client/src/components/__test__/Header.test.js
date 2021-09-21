26440325d41e3c5cbdf58543a88f969cbfc59210
Frank Lam <franlam55@gmail.com>
2021-09-21 16:04:07 +0100

    Test: Refactor Header testing

--- client/src/components/__test__/Header.test.js
+++ client/src/components/__test__/Header.test.js
@@ -1,19 +1,29 @@
-import { render, screen } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Header from "../Header";
 
+let getByTestId;
+
+beforeEach(() => {
+  const component = render(<Header />);
+  getByTestId = component.getByTestId;
+});
+
 describe("component render", () => {
-  it("render header", async () => {
-    render(<Header />);
-
-    const item = screen.getByTestId("header-logo");
-    expect(item).toBeInTheDocument();
+  it("render header", () => {
+    const el = getByTestId("header-logo");
+    expect(el).toBeInTheDocument();
   });
 
-  it("render setting", async () => {
-    render(<Header />);
-
-    const item = screen.getByTestId("setting-logo");
-    expect(item).toBeInTheDocument();
+  it("render setting", () => {
+    const el = getByTestId("header-logo");
+    expect(el).toBeInTheDocument();
   });
+
+  // it("header link", async () => {
+  //   const el = screen.getByTestId("header-logo");
+
+  //   fireEvent.click(el);
+  //   expect(el).toBeInTheDocument();
+  // });
 });
